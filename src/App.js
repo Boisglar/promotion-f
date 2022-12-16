5ed95fe909c9143cd7a4faf9362b6dfeaad45bea
boisglar <dbaisangur95@gmail.com>
2022-12-16 11:05:58 +0300

    сделал компонент для новостей

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import NewsAdd from './components/news/NewsAdd';
+import NewsGet from './components/news/NewsGet';
 
 function App() {
-
+    return (
+        <div className ="App">
+            <NewsGet/>
+            {/* <NewsAdd/> */}
+        </div>
+    )
 }
 
 export default App;
