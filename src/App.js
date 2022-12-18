9205660c736168345f486c9bd52f0b8cd409770d
zelkarnayn <zelkarnayn@gmail.com>
2022-12-18 14:21:12 +0300

    ///

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
-import NewsRender from './components/news/NewsRender';
-// import AddFighter from './components/fighters/AddFighter';
-// import PostNews from './components/news/PostNews';
-import NewsAdd from './components/news/NewsAdd';
-import NewsGet from './components/news/NewsGet';
+// import NewsRender from './components/news/NewsRender';
+import AddFighter from './components/fighters/AddFighter';
+import PostNews from './components/news/PostNews';
+// import NewsAdd from './components/news/NewsAdd';
+// import NewsGet from './components/news/NewsGet';
 
 function App() {
   return (
     <div className="App">
-      <NewsGet/>
-      <NewsRender/>
-      {/* <AddFighter/>
-      <PostNews/> */}
+      {/* <NewsGet/>
+      <NewsRender/> */}
+      <AddFighter/>
+      <PostNews/>
       {/* <NewsAdd/> */}
     </div>
   );
+    }
 
 
 export default App;
