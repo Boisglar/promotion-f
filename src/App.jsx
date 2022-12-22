00d73e93af7c5b322a5a874a8ef39f01f7485619
EHIZIR <chechen.zloi@mail.ru>
2022-12-22 15:03:49 +0300

    сделал верстку

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import Main from './components/main/Main';
+import Header from './components/header/Header';
+import Chemp from './components/chemp/Chemp';
+import Merch from './components/merch/Merch';
+import Life from './components/life/Life';
+import Gallery from './components/gallery/gallery';
+// import {
+//   BrowserRouter as Router,
+//   Route
+// } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <div className='fon'>
+        <Header/>
+        <Main/>
+      </div>
+      <Chemp/>
+      <Merch/>
+      <Life/>
+      {/* <Gallery/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/points/Points.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Points.module.scss'
+
+export default function Points() {
+  return (
+        <div className={styles.uzory}>
+            <div className={styles.uzor}></div>
+            <div className={styles.uzor}></div>
+            <div className={styles.uzor}></div>
+            <div className={styles.uzor}></div>
+        </div>
+  )
+}
